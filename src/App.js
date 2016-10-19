a1cb3cacc14f1e1c2bc3ab94b60871449c350e90
Stavros Bastakis <stavros@webtrails.gr>
2016-10-19 22:03:03 +0300

    Add bootstrap libs to project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Navbar, Grid } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,6 +7,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar inverse fixedTop>
+          <Grid>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <a href="/">React App</a>
+              </Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+          </Grid>
+        </Navbar>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
